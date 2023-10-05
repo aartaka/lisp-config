66091b26bc4c69e0f6cf2f0af4c0eb920f0cb288
Artyom Bologov <aartaka@protonmail.com>
2023-10-06 01:47:18 +0400

    lisp-config: Graven Image configuration.

--- lisp-config.lisp
+++ lisp-config.lisp
@@ -26,6 +26,35 @@
 (asdf:load-system :graven-image)
 (use-package :graven-image)
 
+(defmacro with-useful-printing (&body body)
+  `(let ((*print-case* :downcase)
+	 (*print-level* 2)
+	 (*print-lines* 1)
+	 (*print-length* 7)
+	 (*print-circle* nil))
+     ,@body))
+
+(defmethod gimage:apropos* :around (string &optional package external-only docs-too)
+  (declare (ignorable string package external-only docs-too))
+  (with-useful-printing
+    (call-next-method)))
+
+(defmethod gimage:inspect* :around (object)
+  (declare (ignorable object))
+  (with-useful-printing
+    (call-next-method)))
+
+(defmethod gimage:describe* :around (object &optional (stream t) ignore-methods)
+  "Ignore the `describe-object' methods."
+  (declare (ignorable ignore-methods))
+  (with-useful-printing
+    (call-next-method object stream t)))
+
+(defmethod gimage:documentation* :around (object &optional doc-type)
+  "Implementations throw tantrums getting nonexistent entities' docs."
+  (declare (ignorable object doc-type))
+  (ignore-errors (call-next-method)))
+
 ;;; FIXME: *print-case* :downcase breaks some symbol-generation.
 (setf ;; *print-case* :downcase
       *print-circle* nil
