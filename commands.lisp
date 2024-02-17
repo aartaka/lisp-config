420f738d93455e585db640e71ce6f775e2fdd167
Artyom Bologov <aartaka@protonmail.com>
2024-02-18 03:14:37 +0400

    commands(:page): Add.

--- commands.lisp
+++ commands.lisp
@@ -57,4 +57,32 @@
           (mapcar #'file-namestring
                   (uiop:directory-files (uiop:getcwd)))))
 
-;; TODO: Pager command (:page, :less, :head?)
+(tpl-cmd:define-command/read (:page :pg) (&rest forms)
+  "Page the FORMS.
+
+- If FORMS is a single string, send it to the shell and page program
+  output.
+- If FORMS is a list of Lisp forms, evaluate them and page the
+  *standard-output* used while evaluating."
+  (let* ((to-scroll 10)
+         (output (with-output-to-string (*standard-output*)
+                   (typecase (first forms)
+                     (string (uiop:run-program (first forms) :output t))
+                     (t (eval `(progn ,@forms))))))
+         (lines (uiop:split-string output :separator '(#\Newline))))
+    (labels ((print-page ()
+               (when lines
+                 (loop for i below (min to-scroll (length lines))
+                       do (format t "~&~a" (elt lines i))
+                       finally (setf lines (nthcdr to-scroll lines))))))
+      (loop initially (print-page)
+            while lines
+            for read = (read-line)
+            when (uiop:emptyp read)
+              do (format t "~&~a" (first lines))
+              and do (setf lines (rest lines))
+            else when (char= #\Space (elt read 0))
+                   do (print-page)
+            else when (char-equal #\q (elt read 0))
+                   do (return nil)))))
+
