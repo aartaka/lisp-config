5952f7dc200ed413852fcd9a47f50bbbf446ea4a
Artyom Bologov <aartaka@protonmail.com>
2024-03-03 21:17:08 +0400

    ed: Overwrite to only deal with Lisp forms.

--- ed.lisp
+++ ed.lisp
@@ -2,13 +2,6 @@
   (:use :cl :trivial-toplevel-commands))
 (in-package :editor)
 
-(defmacro define-command/raw (&rest args)
-  `(#-clozure
-    define-command/string
-    #+clozure
-    define-command/eval
-    ,@args))
-
 (defun split-lines (string)
   (uiop:split-string string :separator '(#\Newline)))
 
@@ -17,6 +10,10 @@
 (defvar %ed-stack '())
 (defvar %ed-index 0)
 (defvar %ed-clipboard nil)
+
+(defun print-line ()
+  (format t "~&~d: ~s"
+           %ed-index (elt %ed-buffer %ed-index)))
 
 (define-command (:edit :ed) (&rest to-edit)
   "Edit the TO-EDIT data.
@@ -49,15 +46,13 @@ TO-EDIT might be one of:
              %ed-buffer (gimage:function-lambda-expression* (second head))))
       (cons
        (setf %ed-index 0
-             %ed-buffer (split-lines
-                         (with-output-to-string (*standard-output*)
-                           (eval `(progn ,@to-edit))))))
+             %ed-buffer (eval `(progn ,@to-edit))))
       (integer
        (setf %ed-index
              (max 0 (if (minusp head)
                         (- %ed-index head)
                         (min head (length %ed-buffer)))))))
-    (print-line nil)))
+    (print-line)))
 
 (define-command (:eject :ej) (&optional that-many-lines)
   "Remove THAT-MANY-LINES starting from (and including) the current one."
@@ -77,93 +72,88 @@ TO-EDIT might be one of:
                                        %ed-index
                                        (1+ %ed-index))))))
 
-;; Lisp-only.
 (define-command (:eik :ei) (&optional index)
   "Descend into the current indexed (or form at INDEX) form to edit it."
   (let ((index (or index %ed-index)))
-    (when (listp (elt %ed-buffer %ed-index))
-      (setf %ed-stack (append (list %ed-buffer %ed-index) %ed-stack)
-            %ed-buffer (elt %ed-buffer %ed-index)
-            %ed-index 0))))
+    (when (listp (elt %ed-buffer index))
+      (setf %ed-stack (append (list %ed-buffer index) %ed-stack)
+            %ed-buffer (elt %ed-buffer index)
+            %ed-index 0)
+      (print-line))))
 
-(define-command (:escape :es) (&optional that-much-levels)
+(defun buffer-up (that-much-levels)
+  (loop repeat that-much-levels
+        while %ed-stack
+        for buffer = (pop %ed-stack)
+        for index = (pop %ed-stack)
+        do (setf (elt buffer index) %ed-buffer
+                 %ed-buffer buffer
+                 %ed-index index)))
+
+(define-command (:escape :es) (&optional that-many-levels)
   "Get back to the previous editor state, replacing the previously edited part.
-In case there's THAT-MUCH-LEVELS, pop several levels up."
-  (destructuring-bind (buffer index)
-      (loop repeat (or that-much-levels 1)
-            while %ed-stack
-            for buffer = (pop %ed-stack)
-            for index = (pop %ed-stack)
-            finally (return (list buffer index)))
-      (when index
-        (setf (elt buffer index) %ed-buffer
-              %ed-index index))
-      (setf %ed-buffer buffer)
-      (print-line)))
+In case there's THAT-MANY-LEVELS, pop several levels up."
+  (buffer-up (or that-many-levels 1))
+  (print-line))
 
-;; TODO: Lisp.
-(let ((%query% nil))
-  (define-command (:eavesdrop :ea) (&optional query)
-    "Search for QUERY (or last used query, if not provided) in the buffer."
-    (setf %ed-index (or (position-if (lambda (line)
-                                       (search (or query %query%) line :test #'equal))
-                                     %ed-buffer
-                                     :start %ed-index)
-                        %ed-index)
-          %query% (or query %query%))))
+;; TODO: :eavesdrop (search)
 
-(defun lines-or-read (lines)
-  (if (uiop:emptyp lines)
-      (loop for line = (read-line *standard-input* nil nil)
-            until (string-equal "." line)
-            collect line)
-      (uiop:ensure-list lines)))
+(defun forms-or-read (forms)
+  (if (uiop:emptyp forms)
+      (list (read *standard-input* nil nil))
+      (uiop:ensure-list forms)))
 
 ;; TODO: Better behavior for Lisp vs. Line forms.
-(define-command/raw (:effuse :ef) (&rest forms)
+(define-command (:effuse :ef) (&rest forms)
   "Add new text after the current line."
-  (let ((lines (lines-or-read forms)))
+  (let ((lines (forms-or-read forms)))
     (setf %ed-buffer
           (append (subseq %ed-buffer 0 (1+ %ed-index))
                   lines
                   (subseq %ed-buffer (1+ %ed-index))))
     (incf %ed-index)))
 
-;; TODO: Better behavior for Lisp vs. Line forms.
-(define-command/raw (:embed :em) (&rest forms)
+(define-command (:embed :em) (&rest forms)
   "Add new text before the current line."
-  (let ((lines (lines-or-read forms)))
+  (let ((lines (forms-or-read forms)))
     (setf %ed-buffer
-          (append (subseq %ed-buffer 0 (max 0 (1- %ed-index)))
+          (append (subseq %ed-buffer 0 %ed-index)
                   lines
-                  (subseq %ed-buffer (max 0 (1- %ed-index)))))))
+                  (subseq %ed-buffer %ed-index)))))
 
-;; TODO: Lisp
 (define-command (:enact :en) (&optional object)
   "Save the contents of the editor buffer to OBJECT or current edited object."
-  (when file
-    (setf %ed-file file))
-  (with-open-file (s %ed-file)
-                  (dolist (line %ed-buffer)
-                    (format s "~a~%" line))))
+  (typecase (or object %ed-object)
+    (pathname
+     (with-open-file (s %ed-object :direction :output)
+       (if (member (pathname-type %ed-object)
+                   '("lisp" "lsp" "scm") ;; Scheme!?
+                   :test #'string=)
+           (progn
+             ;; Arbitrary big number. No one will have a 100-levels deep
+             ;; Lisp form, right? Right?
+             (buffer-up 100)
+             (loop for form in %ed-buffer
+                   do (let ((*print-lines* nil)
+                            (*print-length* nil)
+                            (*print-level* nil))
+                        (prin1 form s))))
+           (loop for string in %ed-buffer
+                 do (write-sequence string s)
+                 do (terpri s)))))
+    (function
+     (buffer-up 100)
+     (compile (gimage:function-name* %ed-object) %ed-buffer))))
 
-(defun print-line (detail)
-  (format t "~&~:[~*~a~;~d: ~s~]"
-          detail %ed-index (elt %ed-buffer %ed-index)))
+(define-command (:examine :ex) ()
+  "Print the current line."
+  (print-line))
 
-(define-command (:examine :ex) (&optional detail)
-  "Print the current line depending on DETAIL.
-DETAIL is one of
-- T for listing with line number and standard printing.
-- NIL (default) for regular human-readable printing."
-  (print-line detail))
-
-(define-command (:eye :ey) (&optional detail)
-  "Scroll the editor buffer down `*print-lines*' times, printing them.
-Printing depends on DETAIL, as in :eprint."
+(define-command (:eye :ey) ()
+  "Scroll the editor buffer down `*print-lines*' times, printing them."
   (loop for i below (or *print-lines* 5)
         while (<= %ed-index (1- (length %ed-buffer)))
-        do (print-line detail)
+        do (print-line)
         do (incf %ed-index))
   (setf %ed-index (min (1- (length %ed-buffer)) %ed-index)))
 
