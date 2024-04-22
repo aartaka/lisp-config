80fa779dc1781b399f953c0a768bba90b35a717a
Artyom Bologov <mail@aartaka.me>
2024-04-22 20:14:13 +0400

    config(question-reader): Allow list of (SYMBOL PACKAGE) for apropos

--- config.lisp
+++ config.lisp
@@ -66,7 +66,10 @@ Useful for dependency-based config files."
   (declare (ignorable char arg))
   (let ((val (read stream nil nil t)))
     (typecase val
-      ((or keyword string) (apropos* val))
+      ((or keyword string
+           (cons (or keyword string)))
+       (apropos* (first (uiop:ensure-list val))
+                 (second (uiop:ensure-list val))))
       (symbol (format t "~&~a" (lambda-list* val)))
       (list (format t "~&~a" (documentation* (first val) (second val)))))
     (terpri)
