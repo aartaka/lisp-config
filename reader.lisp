1e65d39bbc12bdb531a59811796dfba7ba3efcba
Artyom Bologov <mail@aartaka.me>
2024-04-24 00:27:22 +0400

    reader(lambda-reader): Dumb down.

--- reader.lisp
+++ reader.lisp
@@ -20,39 +20,15 @@
 
 (defun lambda-reader (stream char arg)
   (declare (ignorable char))
-  (labels ((find-str (str form)
-             (find str form :test #'equal))
-           (fmt-sym (&rest args)
-             (intern (apply #'format nil args) *package*)))
-    (let* ((form (read stream nil nil t))
-           (form-list (uiop:ensure-list form))
-           (arg (or arg
-                    (cond
-                      ((find-str "_9" form-list) 9)
-                      ((find-str "_8" form-list) 8)
-                      ((find-str "_7" form-list) 7)
-                      ((find-str "_6" form-list) 6)
-                      ((find-str "_5" form-list) 5)
-                      ((find-str "_4" form-list) 4)
-                      ((or (find-str "_3" form-list)
-                           (find-str "___" form-list))
-                       3)
-                      ((or (find-str "_2" form-list)
-                           (find-str "__" form-list))
-                       2)
-                      ((or (find-str "_1" form-list)
-                           (find-str "_" form-list))
-                       1)
-                      (t 9))
-                    1)))
-      `(lambda ,(loop for i from 1 upto 9
-                      collect (fmt-sym "_~d" i)
-                        into args
-                      finally (return (append '(&optional) args
-                                              `(&aux ,(list (fmt-sym "_" i) (fmt-sym "_1" i))
-                                                     ,(list (fmt-sym "__" i) (fmt-sym "_2" i))
-                                                     ,(list (fmt-sym "___" i) (fmt-sym "_3" i))))))
-         ,form))))
+  (check-type arg (or null (integer 1 3))
+              "optional number of args (maximum 3)")
+  (let* ((form (read stream nil nil t)))
+    `(lambda ,(append
+               (unless arg
+                 '(&optional))
+               (loop for i from 1 upto (or arg 3)
+                     collect (intern (make-string i :initial-element #\_) *package*)))
+       ,form)))
 
 (set-dispatch-macro-character
  #\# #\^ #'lambda-reader)
