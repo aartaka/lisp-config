a287612156c553be559af0091c205927f7c83485
Artyom Bologov <mail@aartaka.me>
2024-05-03 03:47:57 +0400

    version-control: Add the prototype.

---
+++ version-control.lisp
@@ -0,0 +1,46 @@
+(uiop:define-package :version-control
+  (:use :cl :trivial-toplevel-commands))
+(in-package :version-control)
+
+(define-command (:vivisect :vi) ()
+  "Show the diff of the last commit.
+VIVISECT: cut (a body) open while still alive."
+  (uiop:run-program
+   (list "git" "diff" "HEAD")
+   :output t
+   :error-output t))
+
+(defmacro define-command/raw (&rest args)
+  `(#-clozure
+    define-command/string
+    #+clozure
+    define-command/eval
+    ,@args))
+
+(define-command/eval (:vclone :vc) (repo &optional dirname)
+  "Clone the REPO to DIRNAME or the repo name.
+TODO: NAME THIS COMMAND MNEMONICALLY"
+  (let ((old-dir (uiop:getcwd)))
+    (uiop:run-program
+     (list* "git" "clone"
+            (if (uiop:string-prefix-p "http" repo)
+                repo
+                (uiop:strcat "https://" repo))
+            (when dirname
+              (list dirname)))
+     :output t
+     :error-output t)
+    (uiop:chdir (or dirname
+                    (car (last (uiop:split-string repo :separator (list #\/))))))
+    (uiop:run-program
+     (list "git" "fetch" "--unshallow"))
+    (uiop:chdir old-dir)))
+
+(define-command/string (:vadd :va) (message)
+  "Stage, commit, and send all the changes in the repo."
+  (uiop:run-program
+   (list "git" "add" "--all"))
+  (uiop:run-program
+   (list "git" "commit" "-m" message))
+  (uiop:run-program
+   (list "git" "push")))
